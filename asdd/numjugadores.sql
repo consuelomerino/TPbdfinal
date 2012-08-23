6dbcfda62ff7a05e92ae059a9ae87c3157c6c2ca
Consuelo Merino <consu.mp@gmail.com>
2012-08-23 17:57:09 -0400

    func cant de jugadores en el equipo

---
+++ asdd/numjugadores.sql
@@ -0,0 +1,29 @@
+﻿CREATE OR REPLACE FUNCTION f_equipodejugador(jugador integer)
+RETURNS integer AS
+$BODY$
+declare
+	id integer;
+	numero integer;
+begin
+	id:=(select id_equipo from jugadores 
+			where ci_jugador=jugador);
+	numero:=(select count(*) from jugadores 
+			where id_equipo=id);
+	return numero;
+	end;
+$BODY$
+LANGUAGE plpgsql VOLATILE;
+
+CREATE OR REPLACE FUNCTION f_countequipo(equipo integer)
+RETURNS integer AS
+$BODY$
+declare
+	numero integer;
+begin
+	numero:=(select count(*) from jugadores 
+			where id_equipo=equipo);
+	return numero;
+	end;
+$BODY$
+LANGUAGE plpgsql VOLATILE;
+
