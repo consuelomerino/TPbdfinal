c02977e4bd174e4cb9e487a62faee04df65b9bf3
Consuelo Merino <consu.mp@gmail.com>
2012-08-24 03:22:16 -0400

    ya estan todos los insert faltan los updates ydel
    fijate en el docx

--- asdd/2 triggers.sql
+++ asdd/2 triggers.sql
@@ -1,5 +1,4 @@
-﻿-- triggers
--- funcion que agrega si o si como iniciado el estado del torneo
+﻿--TRIGGER INSERT DE TABLA TORNEOS
 CREATE OR REPLACE FUNCTION f_tbi_torneo() 
 RETURNS TRIGGER 
 AS $$
@@ -24,7 +23,110 @@ $$ LANGUAGE plpgsql;
 CREATE TRIGGER tbi_torneo before insert on torneos for each row
 execute procedure f_tbi_torneo();
 
--- funcion de error para que hora no sea null
+
+
+
+--TRIGGER INSERT DE TABLA EQUIPOS
+CREATE OR REPLACE FUNCTION f_tbi_equipos()
+RETURNS TRIGGER 
+AS $$
+declare
+p_estado estados;
+    BEGIN
+	--solo se puede agregar si no hay algun torneo que este en Proceso ni iniciado
+	p_estado:=(select estado from torneos where estado='EnProceso' limit 1);
+	if p_estado = 'EnProceso' then
+		raise exception 'No se pueden agregar equipos mientras el torneo esté en curso';
+	end if;
+        RETURN NEW;
+    END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER tbi_equipos before insert on equipos for each row
+execute procedure f_tbi_equipos();
+
+
+
+
+
+--TABLA TRIGGER INSERT TABLA JUGADORES
+CREATE OR REPLACE FUNCTION f_tbi_jugadores()
+RETURNS TRIGGER 
+AS $$
+declare
+p_estado estados;
+p_count integer;
+    BEGIN
+	--solo se puede insertar si es que no hay torneos en proceso
+	p_estado:=(select estado from torneos where estado='EnProceso' limit 1);
+	if p_estado = 'EnProceso' then
+		raise exception 'No se pueden agregar equipos mientras el torneo esté en curso';
+	end if;
+	--se pueden insertar maximo 22 jugadores por equipo
+	p_count:=(select count(*) from jugadores where id_equipo=new.id_equipo);
+	if p_count = 22 then
+		raise exception 'Solo se admiten 22 jugadores por equipo';
+	end if;
+        RETURN NEW;
+    END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER tbi_jugadores before insert on jugadores for each row
+execute procedure f_tbi_jugadores();
+
+
+--TRIGGER INSERT TABLA PUNTUACION
+CREATE OR REPLACE FUNCTION f_tbi_puntuacion()
+RETURNS TRIGGER 
+AS $$
+declare
+	p_estado estados;
+    BEGIN
+	--si el estado del torneo es iniciado, se puede insertar, luego ya no
+	p_estado:=(select estado from torneos where anho=new.anho);
+	if p_estado != 'Iniciado' then
+		raise exception 'Una vez iniciado el torneo, no se pueden agregar nuevos registros';
+	end if;
+	new.posicion:=0;
+	new.puntaje:=0;
+        RETURN NEW;
+    END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER tbi_puntuacion before insert on tabla_puntuaciones for each row
+execute procedure f_tbi_puntuacion();
+
+
+
+--TRIGGER INSERT TABLA RONDAS
+CREATE OR REPLACE FUNCTION f_tbi_rondas()
+RETURNS TRIGGER 
+AS $$
+declare
+	p_estado estados;
+	p_fecha fechas;
+    BEGIN
+	p_estado:=(select estado from torneos where anho=new.anho);
+	if p_estado='Terminado' then
+		raise exception 'Torneo ya terminado, no se pueden agregar nuevas rondas!';
+	end if;
+	p_fecha:=(select MAX(fecha) from rondas where anho=new.anho);
+	if p_fecha > new.fecha then
+		raise exception 'Fecha menor a la ultima jugada';
+	end if;
+	if extract(year from new.fecha)!=new.anho then
+		raise exception 'Los anhos deben coincidir';
+	end if;
+        RETURN NEW;
+    END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER tbi_rondas before insert on rondas for each row
+execute procedure f_tbi_rondas();
+
+
+
+--TABLA INSERT CALENDARIO
 CREATE OR REPLACE FUNCTION f_tbiu_calendario()
 RETURNS TRIGGER 
 AS $$
@@ -63,10 +165,9 @@ n_anho integer;
 			raise exception 'partidos se solapan en horario!';
 		end if;
 	end if; 
-	n_anho:=(select t.anho from torneos t
+	n_anho:=(select distinct t.anho from torneos t
 				join rondas r on t.anho=r.anho
-				join calendario c on r.id_ronda=c.id_ronda
-				where c.id_calendario=new.id_calendario);
+				join calendario c on r.id_ronda=new.id_ronda);
 	--solo se pueden agregar hasta 6 canchas
 	c_canchas:=(with r_canchas as (select distinct id_cancha from calendario c
 				join rondas r on c.id_ronda=r.id_ronda
@@ -82,24 +183,41 @@ $$ LANGUAGE plpgsql;
 CREATE TRIGGER tbiu_calendario before insert or update on calendario for each row
 execute procedure f_tbiu_calendario();
 
+
+
+--TRIGGER INSERT PARTIDO
 CREATE OR REPLACE FUNCTION f_tbi_partido()
 RETURNS TRIGGER 
 AS $$
 declare
 rondaactual integer;
 ultimaronda integer;
+estado estados;
     BEGIN
 		--cera los puntajes
 		new.puntaje1=0;
 		new.puntaje2=0;
-
+		--busca el valor de la ronda actual
 		rondaactual:=(select r.id_ronda from rondas r
 				join calendario c on r.id_ronda=c.id_ronda
 				where c.id_calendario=new.id_calendario);
+		--busca el valor de la ultima ronda jugada
 		ultimaronda:=(select MAX(id_ronda) from rondas);
+		--no deja agregar si es que no coincide con la ultima ronda jugada
 		if rondaactual!=ultimaronda then
 			raise exception 'No se puede insertar un partido de otra ronda que no sea la actual';
 		end if;		
+		--si el torneo esta en terminado, no deja agregar mas
+		estado:=(select distinct t.estado from torneos t
+					join rondas r on t.anho=r.anho
+					join calendario c on r.id_ronda=c.id_ronda
+					join partidos p on c.id_calendario=new.id_calendario);
+		if estado = 'Terminado' then
+			raise exception 'No se pueden agregar partidos una vez terminado el torneo';
+		end if;		
+		if new.id_equipo1=new.id_equipo2 then
+			raise exception 'Equipo no puede competir contra si mismo';
+		end if;
         RETURN NEW;
     END;
 $$ LANGUAGE plpgsql;
@@ -109,47 +227,53 @@ execute procedure f_tbi_partido();
 
 
 
-CREATE OR REPLACE FUNCTION f_tbi_rondas()
-RETURNS TRIGGER 
-AS $$
-declare
-	p_estado estados;
-	p_fecha fechas;
-    BEGIN
-	p_estado:=(select estado from torneo where anho=new.anho);
-	if p_estado='Terminado' then
-		raise exception 'Torneo ya terminado, no se pueden agregar nuevas rondas!';
-	end if;
-	p_fecha:=(select MAX(fecha) from rondas where anho=new.anho);
-	if p_fecha > new.fecha then
-		raise exception 'Fecha menor a la ultima jugada';
-	end if;
-        RETURN NEW;
-    END;
-$$ LANGUAGE plpgsql;
-
-CREATE TRIGGER tbi_rondas before insert on rondas for each row
-execute procedure f_tbi_rondas();
-
-
-
-
-
+--TRIGGER INSERT TABLA PLANILLA
 CREATE OR REPLACE FUNCTION f_tbi_planilla()
 RETURNS TRIGGER 
 AS $$
 DECLARE 
 equipo integer;
+estado estados;
+rondaactual integer;
+ultimaronda integer;
+canttitular integer;
     BEGIN
 	--si o si tiene que ser titular (true) o suplente (false)
 	if new.fue_titular IS NULL then
 		raise exception 'Debe especificar si es Titular, "1" o Suplente, "0"';
 	end if;
 	--corroborar que el jugador es del equipo
-	equipo:=(select distinct p.id_equipo from planillas p join jugadores j 
-					on j.ci_jugador = new.ci_jugador and p.id_equipo = j.id_equipo);
-	if new.id_equipo != equipo then
+	equipo:=(select distinct j.id_equipo from planillas p join jugadores j 
+					on j.ci_jugador = new.ci_jugador);
+	if new.id_equipo != equipo or equipo = NULL then
 		raise exception 'Jugador no válido, no asociado al equipo ingresado';
+	end if;
+	estado:=(select distinct t.estado from torneos t
+					join rondas r on t.anho=r.anho
+					join calendario c on r.id_ronda=c.id_ronda
+					join partidos p on c.id_calendario=p.id_calendario
+					where p.id_partido=new.id_partido);
+	--si el torneo ya termino, no se puede insertar
+	if estado = 'Terminado' then
+		raise exception 'No se pueden agregar partidos una vez terminado el torneo';
+	end if;		
+	--valor de la ronda actual
+	rondaactual:=(select r.id_ronda from rondas r
+			join calendario c on r.id_ronda=c.id_ronda
+			join partidos p on c.id_calendario=p.id_calendario
+			where p.id_partido=new.id_partido);
+	--busca el valor de la ultima ronda jugada
+	ultimaronda:=(select MAX(id_ronda) from rondas);
+	--no deja agregar si es que no coincide con la ultima ronda jugada
+	if rondaactual!=ultimaronda then
+		raise exception 'No se puede insertar un partido de otra ronda que no sea la actual';
+		end if;	
+	--solo puede haber un maximo de 11 titulares en el partido
+	canttitular:=(select count(*) from planillas 
+			where id_equipo=equipo and fue_titular=true
+				and id_partido=new.id_partido);
+	if canttitular>11 then
+		raise exception 'No pueden haber más de 11 titulares en el equipo en el mismo partido';
 	end if;
 	RETURN NEW;
     END;
@@ -158,14 +282,20 @@ $$ LANGUAGE plpgsql;
 CREATE TRIGGER tbi_planilla before insert on planillas for each row
 execute procedure f_tbi_planilla();
 
+
+--TRIGGER INSERT TABLA GOLES_X_JUGADOR
 CREATE OR REPLACE FUNCTION f_tbiu_goles()
 RETURNS TRIGGER 
 AS $$
 DECLARE
-p_fecha date;
-p_jugador integer;
-a_fecha date;
-p_partido record;
+	equipo integer;
+	p_fecha date;
+	p_jugador integer;
+	a_fecha date;
+	p_partido record;
+	rondaactual integer;
+	estado estados;
+	p_titulares integer;
     BEGIN
 	if TG_OP='INSERT' or TG_OP='UPDATE' then
 		--ver que los goles que se inserten son de la fecha actual
@@ -175,7 +305,8 @@ p_partido record;
 				join calendario c on pa.id_calendario=c.id_calendario
 				join rondas r on c.id_ronda=r.id_ronda 
 				and p.id_planilla=new.id_planilla limit 1);
-		a_fecha:=(select fecha from rondas where id_ronda=currval('rondas_id_ronda_seq'));
+		rondaactual:=(select MAX(id_ronda) from rondas);
+		a_fecha:=(select fecha from rondas where id_ronda=rondaactual);
 
 		if not(p_fecha = a_fecha) then
 			raise exception 'No se pueden agregar goles fuera de fecha';
@@ -183,13 +314,39 @@ p_partido record;
 		--cerar los resultados de los goles del jugador si es que no se pasa algún parámetro
 	end if;
 	if TG_OP='INSERT' then
+		--se cera a favor y en contra si es que los campos estan como null
 		if new.a_favor IS NULL then
 			new.a_favor=0;
 		end if;
 		if new.contra IS NULL then
 			new.contra=0;
 		end if;
+		--si es que no tiene al menos 11 jugadores titulares, no se puede comenzar a asignar los goles
+		p_jugador:=(select ci_jugador from planillas where id_planilla=new.id_planilla); 
+		equipo:=(select distinct j.id_equipo from planillas p join jugadores j 
+					on j.ci_jugador = p_jugador);
+		p_titulares:=(select count(*) from planillas 
+				where fue_titular=TRUE and id_equipo=equipo);
+		if p_titulares < 11 then
+			raise exception 'ERROR!! La cantidad de titulares debe ser al menos 11';
+		end if;
 	end if;
+	--si es un update, se restringe el cambio de id_goles e id_planilla
+	if TG_OP='UPDATE' then
+		if new.id_goles!=old.id_goles and new.id_planilla!=old.id_planilla then
+			raise exception 'NO se puede cambiar el id de goles y id de planilla';
+		end if;
+	end if;
+	--si torneo finalizado, no se puede insertar ni actualizar
+	estado:=(select distinct t.estado from torneos t
+					join rondas r on t.anho=r.anho
+					join calendario c on r.id_ronda=c.id_ronda
+					join partidos p on c.id_calendario=p.id_calendario
+					join planillas pa on p.id_partido=pa.id_partido
+					where pa.id_planilla=new.id_planilla);
+	if estado = 'Terminado' then
+		raise exception 'No se pueden agregar partidos una vez terminado el torneo';
+	end if;		
         RETURN NEW;
     END;
 $$ LANGUAGE plpgsql;
@@ -208,14 +365,9 @@ DECLARE
 	p_idequipo integer;
 	p_equipo1 integer;
 	p_equipo2 integer;
-	p_titulares integer;
+
 BEGIN
-		--si es que no tiene al menos 11 jugadores titulares, no se puede comenzar a asignar los goles
-		p_titulares:=(select count(*) from planillas 
-				where fue_titular=TRUE and id_equipo=1new.id_equipo);
-		if p_titulares < 11 then
-			raise exception 'ERROR!! La cantidad de titulares debe ser al menos 11';
-		end;
+
 		--rescata el id del jugador que metio el gol
 		p_cijugador:=(select distinct p.ci_jugador from planillas p
 				where p.id_planilla=new.id_planilla);
@@ -339,24 +491,3 @@ $$ LANGUAGE plpgsql;
 CREATE TRIGGER tad_goles after delete on goles_x_jugador for each row
 execute procedure f_tad_goles();
 
-
-CREATE OR REPLACE FUNCTION f_tbi_puntuacion()
-RETURNS TRIGGER 
-AS $$
-declare
-	p_estado estados;
-    BEGIN
-
-	--si el estado del torneo es iniciado, se puede insertar, luego ya no
-	p_estado:=(select estado from torneo where anho=new.anho);
-	if p_estado != 'Iniciado' then
-		raise exception 'Una vez iniciado el torneo, no se pueden agregar nuevos registros';
-	end if;
-	new.posicion:=0;
-	new.puntaje:=0;
-        RETURN NEW;
-    END;
-$$ LANGUAGE plpgsql;
-
-CREATE TRIGGER tbi_puntuacion before insert on tablapuntuacion for each row
-execute procedure f_tbi_partido();
---
+++ asdd/queries de prueba de consistencia.sql
@@ -0,0 +1,150 @@
+﻿--querys de prueba de consistencia
+--TABLA TORNEO
+insert into torneos (anho, estado, nombre_torneo) values (2001, 'Iniciado', 'Ala');
+--da error por que hay uno con iniciado 
+insert into torneos (anho, estado, nombre_torneo) values (2002, 'Iniciado', 'Ala');
+update torneos set estado='Terminado' where anho=2001;
+--da error anho anterior
+insert into torneos (anho, estado, nombre_torneo) values (2000, 'Iniciado', 'Ala');
+select * from torneos;
+insert into torneos (anho, estado, nombre_torneo) values (2003, 'Iniciado', 'Ala1');
+--TABLA EQUIPO
+--no se puede insertar porque hay un torneo Iniciado
+insert into equipos (nombre_equipo) values ('Olimpia');
+
+--TABLA CANCHA
+insert into canchas (nombre_cancha, lugar) values ('la O', 'Avda. Eusebio');
+insert into canchas (nombre_cancha, lugar) values ('la 1', 'Avda. Eusebio');
+insert into canchas (nombre_cancha, lugar) values ('la 2', 'Avda. Eusebio');
+insert into canchas (nombre_cancha, lugar) values ('la 3', 'Avda. Eusebio');
+insert into canchas (nombre_cancha, lugar) values ('la 4', 'Avda. Eusebio');
+insert into canchas (nombre_cancha, lugar) values ('la 5', 'Avda. Eusebio');
+insert into canchas (nombre_cancha, lugar) values ('la 6', 'Avda. Eusebio');
+select * from canchas;
+--TABLA JUGADORES
+--error por torneo que está en curso
+insert into jugadores (ci_jugador, id_equipo, apellido, edad, nombre) values 
+			(12353, 1, 'Gonzalez', 18, 'Pedro');
+update torneos set estado='Iniciado' where anho=2003;
+insert into jugadores (ci_jugador, id_equipo, apellido, edad, nombre) values 
+			(12353, 1, 'Gonzalez', 18, 'Pedro');
+--TABLA PUNTUACIONES
+--no se puede insertar si el torneo a la que se le asignan las puntuaciones no esta en iniciado
+insert into tabla_puntuaciones (id_equipo, anho) values
+			(1, 2001);
+insert into tabla_puntuaciones (id_equipo, anho) values
+			(1, 2003);
+--TABLA RONDAS
+insert into rondas (fecha, anho, numero_ronda) values ('12-13-2003', 2003, 1);
+--no acepta que los anhos no coincidan
+insert into rondas (fecha, anho, numero_ronda) values ('12-13-2004', 2003, 2);
+--no acepta una fecha anterior a la ya jugada
+insert into rondas (fecha, anho, numero_ronda) values ('12-13-2002', 2003, 2);
+select * from rondas;
+--TABLA CALENDARIO
+insert into calendario (id_ronda, id_cancha, hora) values (4,1,'12:00');
+insert into calendario (id_ronda, id_cancha, hora) values (4,1,'18:00');
+--solo se pueden jugar dos partidos por cancha
+insert into calendario (id_ronda, id_cancha, hora) values (4,1,'17:00');
+insert into calendario (id_ronda, id_cancha, hora) values (4,2,'17:00');
+--se solapan en horario
+insert into calendario (id_ronda, id_cancha, hora) values (11,2,'18:00');
+insert into calendario (id_ronda, id_cancha, hora) values (11,5,'17:00');
+insert into calendario (id_ronda, id_cancha, hora) values (11,6,'17:00');
+insert into calendario (id_ronda, id_cancha, hora) values (11,7,'17:00');
+insert into calendario (id_ronda, id_cancha, hora) values (11,3,'17:00');
+insert into calendario (id_ronda, id_cancha, hora) values (11,4,'20:00');
+--no pueden haber mas de 6 canchas en utilizacion por ronda
+insert into calendario (id_ronda, id_cancha, hora) values (11,1,'20:00');
+--TABLE PARTIDO
+--no puede competir contra si mismo
+insert into partidos (id_calendario, id_equipo1, id_equipo2) values
+			(31, 1, 1);
+--no puede insertar un partido de una ronda que no sea la actual
+insert into partidos (id_calendario, id_equipo1, id_equipo2) values
+			(3, 1, 2);
+update torneos set estado='Terminado' where anho=2003;
+--no se pueden agregar partidos una vez terminado el torneo
+insert into partidos (id_calendario, id_equipo1, id_equipo2) values
+			(31, 1, 1);
+--TABLE PLANILLAS
+select * from jugadores;
+select * from partidos;
+--no se puede agregar un jugador cuando termina el torneo
+insert into planillas (id_partido, ci_jugador, fue_titular, id_equipo) values
+			(8, 12353, TRUE, 1);
+update torneos set estado='EnProceso' where anho=2004;
+--no se puede agregar un jugador que no pertenece a dicho equipo
+insert into planillas (id_partido, ci_jugador, fue_titular, id_equipo) values
+			(8, 12353, TRUE, 1);
+--no se puede agregar un jugador en la ronda que no es actual
+insert into planillas (id_partido, ci_jugador, fue_titular, id_equipo) values
+			(2, 12353, TRUE, 1);
+update torneos set estado='Iniciado' where anho=2004;
+insert into jugadores (ci_jugador, id_equipo, apellido, edad, nombre) values 
+			(1, 1, 'Gonzalez', 18, 'Pedro');
+insert into jugadores (ci_jugador, id_equipo, apellido, edad, nombre) values 
+			(2, 1, 'Gonzalez', 18, 'Pedro');
+insert into jugadores (ci_jugador, id_equipo, apellido, edad, nombre) values 
+			(3, 1, 'Gonzalez', 18, 'Pedro');
+insert into jugadores (ci_jugador, id_equipo, apellido, edad, nombre) values 
+			(4, 1, 'Gonzalez', 18, 'Pedro');
+insert into jugadores (ci_jugador, id_equipo, apellido, edad, nombre) values 
+			(5, 1, 'Gonzalez', 18, 'Pedro');
+insert into jugadores (ci_jugador, id_equipo, apellido, edad, nombre) values 
+			(6, 1, 'Gonzalez', 18, 'Pedro');
+insert into jugadores (ci_jugador, id_equipo, apellido, edad, nombre) values 
+			(7, 1, 'Gonzalez', 18, 'Pedro');
+insert into jugadores (ci_jugador, id_equipo, apellido, edad, nombre) values 
+			(8, 1, 'Gonzalez', 18, 'Pedro');
+insert into jugadores (ci_jugador, id_equipo, apellido, edad, nombre) values 
+			(9, 1, 'Gonzalez', 18, 'Pedro');
+insert into jugadores (ci_jugador, id_equipo, apellido, edad, nombre) values 
+			(10, 1, 'Gonzalez', 18, 'Pedro');
+insert into jugadores (ci_jugador, id_equipo, apellido, edad, nombre) values 
+			(11, 1, 'Gonzalez', 18, 'Pedro');
+insert into jugadores (ci_jugador, id_equipo, apellido, edad, nombre) values 
+			(12, 1, 'Gonzalez', 18, 'Pedro');
+insert into planillas (id_partido, ci_jugador, fue_titular, id_equipo) values
+			(8, 1, TRUE, 1);
+insert into planillas (id_partido, ci_jugador, fue_titular, id_equipo) values
+			(8, 2, TRUE, 1);
+insert into planillas (id_partido, ci_jugador, fue_titular, id_equipo) values
+			(8, 3, TRUE, 1);
+insert into planillas (id_partido, ci_jugador, fue_titular, id_equipo) values
+			(8, 4, TRUE, 1);
+insert into planillas (id_partido, ci_jugador, fue_titular, id_equipo) values
+			(8, 5, TRUE, 1);
+insert into planillas (id_partido, ci_jugador, fue_titular, id_equipo) values
+			(8, 6, TRUE, 1);
+insert into planillas (id_partido, ci_jugador, fue_titular, id_equipo) values
+			(8, 7, TRUE, 1);
+insert into planillas (id_partido, ci_jugador, fue_titular, id_equipo) values
+			(8, 8, TRUE, 1);
+insert into planillas (id_partido, ci_jugador, fue_titular, id_equipo) values
+			(8, 9, TRUE, 1);
+insert into planillas (id_partido, ci_jugador, fue_titular, id_equipo) values
+			(8, 10, TRUE, 1);
+--no se puede agregar más de 11 titulares
+insert into planillas (id_partido, ci_jugador, fue_titular, id_equipo) values
+			(8, 11, TRUE, 1);
+insert into planillas (id_partido, ci_jugador, fue_titular, id_equipo) values
+			(8, 12, TRUE, 1);
+select * from planillas
+--TABLA GOLES
+--no se pueden agregar goles de otra ronda
+insert into goles_x_jugador (id_planilla, a_favor, contra) values
+			(11, 0, 0);
+--si es que tiene menos de 11, no agrega
+insert into goles_x_jugador (id_planilla, a_favor, contra) values
+			(7, 0, 0);
+
+
+
+--UPDATES
+
+
+
+
+
+
